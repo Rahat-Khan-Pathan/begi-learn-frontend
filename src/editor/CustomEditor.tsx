c159317661fffb275efeb87ea5160fdea5f7ce12
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2024-06-03 02:40:50 +0600

    netlify redirects issue fixes

--- src/editor/CustomEditor.tsx
+++ src/editor/CustomEditor.tsx
@@ -1,7 +1,9 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import "ckeditor5-custom-build/build/ckeditor";
-import "ckeditor5-custom-build/sample/styles.css";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic/build/ckeditor"
+import "../../ckeditor5/sample/styles.css";
+
 interface IProps {
     text: string;
     setText: Dispatch<SetStateAction<string>>;
@@ -12,7 +14,7 @@ const CustomEditor = ({ text, setText, error, setError }: IProps) => {
     return (
         <div style={{border: error ? "1px solid red" : ""}}>
             <CKEditor
-                editor={window['ClassicEditor']}
+                editor={ClassicEditor}
                 data={text}
                 onChange={(_, editor) => {
                     const data = editor.getData();
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
     return {
+        plugins: [react()],
         optimizeDeps: {
-            include: ["@workspace/ckeditor5-custom-build"],
+            include: ["ckeditor5-custom-build"],
         },
         build: {
             commonjsOptions: {
-                include: [/@workspace\/ckeditor5-custom-build/, /node_modules/],
+                exclude: ["ckeditor5-custom-build"],
             },
         },
-        plugins: [react()],
     };
 });
